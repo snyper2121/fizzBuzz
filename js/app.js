7e81d50487f7aaf67c744a556b03b20e21b51b39
snyper2121 <gaby4b11@gmail.com>
2016-09-11 21:25:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 for (var i = 1; i <= 100; i++) {
 	document.write("<br>");
-	if (i % 15 == 0) {
+	if (i % 15 === 0) {
 		document.write("FizzBuzz");
-	} else if (i % 3 == 0) {
+	} else if (i % 3 === 0) {
 		document.write("Fizz");
-	} else if (i % 5 == 0) {
+	} else if (i % 5 === 0) {
 		document.write("Buzz");
 	} else {
 		document.write(i);
